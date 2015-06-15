059c310551c3d2853dc04a4fabbaa0561f0d70d5
aranair <boa.homan@gmail.com>
2015-06-15 17:55:18 +0800

    URL query working

--- main.go
+++ main.go
@@ -9,17 +9,23 @@ import (
 
 func main() {
 	router := httprouter.New()
+	// router.GET("/cafes/location/:location", Index)
 	router.GET("/cafes", Index)
 	router.GET("/cafes/:id", Show)
 
 	// log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println("Server starting at port 8080.")
 	http.ListenAndServe(":8080", router)
-	fmt.Println("Server started at port 8080.")
 }
 
 func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
-	fmt.Println(ps)
+
+	query := r.URL.Query()
+	if len(query["location"]) > 0 {
+		lq := query["location"][0]
+		fmt.Fprint(w, "Your location query: ", lq)
+	}
 }
 
 func Show(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
