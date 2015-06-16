b899c7ad836ad7c24e4c5b162a10250d93d0af49
Homan <boa.homan@gmail.com>
2015-06-17 02:18:13 +0800

    toml and db, before db works

--- app/main.go
+++ app/main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"database/sql"
 
 	gc "github.com/aranair/gocafes"
 	router "github.com/aranair/gocafes/router"
+
+	_ "github.com/lib/pq"
+
+	"github.com/BurntSushi/toml"
 	"github.com/justinas/alice"
 )
 
-func main() {
-	ac := gc.NewAppContext()
-	stack := alice.New()
-	r := router.New()
+type Config struct {
+	DB database `toml:"database"`
+}
 
+type database struct {
+	User     string
+	Password string
+}
+
+type Cafe struct {
+	Address string
+	Name    string
+	Score   float32
+	Id      int
+}
+
+func main() {
+	var conf Config
+	if _, err := toml.DecodeFile("configs.toml", &conf); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(conf)
+
+	// pqStr := "user=" + conf.DB.User + " password=" + conf.DB.Password + " dbname=gocafe sslmode=verify-full"
+	db, err := sql.Open("postgres", "postgres://postgres:8gmytbz7***@localhost/gocafe?sslmode=verify-full")
+	// db, err := sql.Open("postgres", pqStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	// var cafe Cafe
+	rows, err := db.Query("SELECT id FROM cafes WHERE id = $1", 1)
+	fmt.Println(rows)
+	// fmt.Println(cafe)
+
+	ac := gc.NewAppContext(db)
+	stack := alice.New()
+
+	r := router.New()
 	r.GET("/cafes", stack.ThenFunc(ac.IndexHandler))
 	r.GET("/cafes/:id", stack.ThenFunc(ac.ShowHandler))
 
