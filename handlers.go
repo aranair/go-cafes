622e9173c2f3ee1764af3ee87cea883716eb162d
Homan <boa.homan@gmail.com>
2015-06-16 17:08:37 +0800

    Routers and handlers separate

--- handlers.go
+++ handlers.go
@@ -1,1 +1,31 @@
-package main
+package gocafes
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/context"
+	"github.com/julienschmidt/httprouter"
+)
+
+type AppContext struct {
+	db *sql.DB
+}
+
+// TODO: Add in db app context
+func NewAppContext() AppContext {
+	return AppContext{nil}
+}
+
+func (ac *AppContext) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+	fmt.Println(context.Get(r, "params"))
+	params := context.Get(r, "params").(httprouter.Params)
+	fmt.Println(params.ByName("location"))
+}
+
+func (ac *AppContext) ShowHandler(w http.ResponseWriter, r *http.Request) {
+	params := context.Get(r, "params").(httprouter.Params)
+	fmt.Fprintf(w, "hello, %s!\n", params.ByName("id"))
+}
