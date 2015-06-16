d2013d4b7d59fb5a239601579633901a06eaffb5
Homan <boa.homan@gmail.com>
2015-06-16 17:09:39 +0800

    Print starting app message before

--- app/main.go
+++ app/main.go
@@ -12,11 +12,11 @@ import (
 func main() {
 	ac := gc.NewAppContext()
 	stack := alice.New()
-
 	r := router.New()
+
 	r.GET("/cafes", stack.ThenFunc(ac.IndexHandler))
 	r.GET("/cafes/:id", stack.ThenFunc(ac.ShowHandler))
 
+	fmt.Println("Server starting at port 8080.")
 	http.ListenAndServe(":8080", r)
-	fmt.Println("Server started at port 8080.")
 }
